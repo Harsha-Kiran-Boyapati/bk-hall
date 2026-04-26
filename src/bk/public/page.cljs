20e6fa2871399e9621f3eda375752eb601fd781c
Harsha <harsha.boyapati@harness.io>
2026-04-26 14:13:52 +0530

    feat: add amenities section

--- src/bk/public/page.cljs
+++ src/bk/public/page.cljs
@@ -1,8 +1,10 @@
 (ns bk.public.page
   (:require [bk.public.hero :as hero]
-            [bk.public.gallery :as gallery]))
+            [bk.public.gallery :as gallery]
+            [bk.public.amenities :as amenities]))
 
 (defn root []
   [:div
    [hero/section]
-   [gallery/section]])
+   [gallery/section]
+   [amenities/section]])
