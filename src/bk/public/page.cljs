8b95be6b8fa1a331075c22e6f2a426c360b8656d
Harsha <harsha.boyapati@harness.io>
2026-04-26 14:14:46 +0530

    feat: add availability calendar

--- src/bk/public/page.cljs
+++ src/bk/public/page.cljs
@@ -2,11 +2,13 @@
   (:require [bk.public.hero :as hero]
             [bk.public.gallery :as gallery]
             [bk.public.amenities :as amenities]
-            [bk.public.pricing :as pricing]))
+            [bk.public.pricing :as pricing]
+            [bk.public.calendar :as calendar]))
 
 (defn root []
   [:div
    [hero/section]
    [gallery/section]
    [amenities/section]
-   [pricing/section]])
+   [pricing/section]
+   [calendar/section]])
